e2fa7e2ba41a3a8738a891a7821dc416c52a3592
Mark Bruckert <markbruckert1@gmail.com>
2019-01-11 17:38:48 -0500

    Added Hover Effects

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Play from './play.png';
 import ProductOne from './productone.png';
 import ProductTwo from './producttwo.png';
 import ProductThree from './productthree.png';
+import Popup from "reactjs-popup";
+import { HashRouter } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
+const App = () => (
       <div className="App">
       <img src={band} className="band" alt="band" />
         <header className="App-header">
@@ -16,15 +17,33 @@ class App extends Component {
           <p className="App-intro"> #1 Rock Band in the Country</p>
         </header>
 
+
         <h2>LATEST HITS</h2>
-        <div className="songone">
+
+      <Popup trigger={<div className="songone">
         <img className="play" src={Play}/>
         <h3>The Middle</h3>
-        </div>
-        <div className="songtwo">
+        </div>} position="bottom center" on="hover">
+      <div>
+        <br/>
+        <a href="https://open.spotify.com/artist/2tKdnNrll3phfqxSva3qdj" target="_black"><img className="spotify" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png"/></a>
+        <br/>
+        <img className="apple" src="https://cdn.dribbble.com/users/124355/screenshots/2129607/itunes.png"/>
+      </div>
+      </Popup>
+
+      
+      <Popup trigger={<div className="songtwo">
         <img className="play" src={Play}/>
         <h3>Flexbit</h3>
-        </div>
+        </div>} position="bottom center" on="hover">
+      <div>
+      <br/>
+        <a href="https://open.spotify.com/artist/2tKdnNrll3phfqxSva3qdj" target="_black"><img className="spotify" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/1024px-Spotify_logo_without_text.svg.png"/></a>
+        <br/>
+        <img className="apple" src="https://cdn.dribbble.com/users/124355/screenshots/2129607/itunes.png"/>
+      </div>
+      </Popup>
       
         <h2>NEWS</h2>
       <div className="blog">
@@ -61,7 +80,18 @@ class App extends Component {
 
       </div>
     );
-  }
-}
+
+    const Blog = () => (
+      <div className="App">
+      <h1>Hello!</h1>
+      </div>
+    );
+
+const BasicExample = () => (
+  <HashRouter>
+    <Route exact path="/" component={App}/>
+    <Route path="/blog" component={Blog}/>
+  </HashRouter>
+);
 
 export default App;
